9e09a972df489a6143d8c1f1a326d9b47a9e9ab1
oliviawu <oliviawu0301@gmail.com>
2024-10-14 09:32:45 +0800

    矩形含對角練習

--- pra03.php
+++ pra03.php
@@ -149,10 +149,64 @@ for($i=0;$i<21;$i++){
 
 }
 ?>
-
-<h2>矩形123</h2>
- 
+<h2>矩形</h2>
  <?php 
+for($i=0;$i<5;$i++){
+    for($j=0;$j<5;$j++){
+        if($i==0 || $i==4){
+            echo "*";    
+        }else if($j==0 || $j==4){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }  
+    }
+    echo "<br>";
+
+}
+
+
+?>
+
+<h2>矩形含對角線練習</h2>
+ <?php 
+ /*奇數*/
+
+for($i=0;$i<5;$i++){
+    for($j=0;$j<5;$j++){
+        if($i==0 || $i==4){
+            echo "*";    
+        }else if($j==0 || $j==4){
+            echo "*";
+        }else if($i==$j ||$j==(4-$i)){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }  
+    }
+    echo "<br>";
+}
+
+?>
+
+<h2>矩形含對角線練習</h2>
+ <?php 
+ /*偶數*/
+
+for($i=0;$i<6;$i++){
+    for($j=0;$j<6;$j++){
+        if($i==0 || $i==5){
+            echo "*";    
+        }else if($j==0 || $j==5){
+            echo "*";
+        }else if($i==$j ||$j==(5-$i)){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }  
+    }
+    echo "<br>";
+}
 
 ?>
 
